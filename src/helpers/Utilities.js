cb3ab2a41190ce545f19954d51d672a50945f285
imran.memon <imran.memon@waqartech.com>
2020-03-08 20:29:09 +0500

    local storage

--- src/helpers/Utilities.js
+++ src/helpers/Utilities.js
@@ -3,6 +3,32 @@ import Storage from './Storage';
 export let stringIsEmpty = (str) => {
     return (!str || /^\s*$/.test(str));
 };
+
+export let asyncStorage_SaveKey = async (key, value) => {
+    try {
+        await AsyncStorage.setItem(key, value);
+    } catch (error) {
+        console.log("Error saving data" + error);
+    }
+}
+
+export let asyncStorage_GetKey = async (key) => {
+    try {
+        const value = await AsyncStorage.getItem(key);
+        return value;
+    } catch (error) {
+        console.log("Error retrieving data" + error);
+    }
+}
+
+export let asyncStorage_RemoveKey = async (key) => {
+    try {
+        await AsyncStorage.removeItem(key);
+    } catch (error) {
+        console.log("Error resetting data" + error);
+    }
+}
+
 export let setHeaders = () => {
     try{
         if(Object.keys(Storage.userData).length > 0){

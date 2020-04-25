43812cc4817c4e291b0fe102b05fc14af0bc8441
imran.memon <imran.memon@waqartech.com>
2020-04-25 14:12:13 +0500

    file added

---
+++ src/services/Messages.js
@@ -0,0 +1,22 @@
+import APIConstants from '../helpers/APIConstants';
+import * as Utilities from "../helpers/Utilities";
+import Storage from '../helpers/Storage';
+
+export let MyConversations = async () => {
+    try {
+        if(Storage.networkStatus.isConnected){
+        let response = await fetch(APIConstants.GET_MY_CONVERSATIONS_ENDPOINT, {
+            method: 'GET',
+            headers: Utilities.setHeaders()
+        })
+        let result = response.json()
+        return result;
+    }else{
+        return null
+    }
+    } catch (e) {
+        console.log(e.message)
+        return null
+    }
+}
+
